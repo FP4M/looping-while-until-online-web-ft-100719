822333882e0a0df184492b240b2d879672711386
Prince Charles <princecharles35@yahoo.com>
2019-10-10 18:08:37 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++
@@ -1,7 +0,0 @@
-describe "#using_until" do
-	it "puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
-	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	expect{ using_until }.to output(looping_string).to_stdout
-	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	  expect{ using_until }.to output(looping_string).to_stdout
-  end
